f450eed01afb46900f1a0d0b31b7926d3cbeb544
John Steele <programjsteele@gmail.com>
2010-09-29 12:26:12 -0700

    Added header with device info.

---
+++ src/sleepy.h
@@ -0,0 +1,22 @@
+
+#ifndef _SLEEPY_H_
+#define _SLEEPY_H_
+
+#ifndef MY_NUM_DEVICES
+#define MY_NUM_DEVICES 1 /* 1 to start */
+#endif
+
+#ifndef MY_MAJOR
+#define MY_MAJOR 0    
+#endif
+
+#ifndef MY_MINOR
+#define MY_MINOR 0
+#endif 
+
+extern int my_major;
+extern int my_minor;
+extern int my_num_devices;
+
+#endif /* _SLEEPY_H_ */
+
