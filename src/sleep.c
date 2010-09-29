b5c775adfccfcb9efc4b20dbfda020bb5a1ef8a9
John Steele <programjsteele@gmail.com>
2010-09-29 00:39:25 -0700

    Added a simple Makefile and got it to compile.

--- src/sleep.c
+++ src/sleep.c
@@ -7,25 +7,25 @@
 
 
 
-struct file_operations fops = {
-	.owner = THIS_MODULE,	
-	.read  = device_read,
-	.write = device_write,
-};
-
-
 ssize_t device_read(struct file *filp, char __user *buff, size_t count,
 			loff_t *f_pos)
 {
-
+	return count;
 }
 
 
 ssize_t device_write(struct file *filp, const char __user *buff, size_t count, 
 			loff_t *f_pos)
 {
-
+	return count;
 } 
+
+
+struct file_operations fops = {
+	.owner = THIS_MODULE,	
+	.read  = device_read,
+	.write = device_write,
+};
 
 
 void __exit device_cleanup(void) 
@@ -36,10 +36,16 @@ void __exit device_cleanup(void)
 
 int __init device_init(void)
 {
-
+	int result;
+	result = 0;
+	return result;
 }
 
 
 module_init(device_init);
 module_exit(device_cleanup);
+
+
+MODULE_AUTHOR("John Steele");
+MODULE_LICENSE("DUAL BSD/GPL");
 
